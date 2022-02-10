76f6813b18ac3440287660bf10bba8ed9f01d9bd
Vaios Karampinis <oden@oden.gr>
2022-02-10 09:53:08 +0200

    add hero image to the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,44 @@
-import BaseLayout from "../components/BaseLayout";
+import Body from "../components/Body";
+import Button from "../components/Button";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import BalloonsIcon from "../components/Icons/BalloonsIcon";
 
 export default function Home() {
   return (
-    <BaseLayout>
-      <Welcome />
-    </BaseLayout>
+    <Body>
+      <Header />
+      <Hero />
+      <Footer />
+    </Body>
   );
 }
 
-function Welcome() {
+function Hero() {
   return (
-    <div className="md:flex">
-      <div className="md:flex-initial">
-        <img
-          className="mb-6 lg:pt-5"
-          src="images/kid-playing.png"
-          alt="kid playing"
-        />
+    <section className="relative w-full h-screen">
+      <img
+        src="/images/hero-splash-1.jpeg"
+        class="object-cover w-full h-full"
+        alt="Image alt text"
+      />
+      <div className="absolute left-16 top-48">
+        <div className="p-10 mb-8 bg-white shadow-lg rounded-xl">
+          <span className="block mb-2 text-2xl font-bold ">Τηλεθεραπεία</span>
+          <span className="block mb-12 text-2xl">
+            Η λογοθεραπεία σήμερα είναι απλά ένα κλικ μακριά
+          </span>
+          <div className="text-center">
+            <a href="#" className="p-4 text-white bg-pink-600 rounded-lg">
+              Δωρεάν αξιολόγηση
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="px-10 py-4 prose lg:flex-1 lg:prose-xl">
-        <h2>Καλώς ήρθατε</h2>
-        <p>
-          Ο χώρος "Σκέψη - Λέξη", δημιουργήθηκε με σκοπό την παροχή υπηρεσιών
-          λογοθεραπείας που απευθύνεται αποκλειστικά σε παιδιά. Υπεύθυνη του
-          χώρου, είναι η λογοθεραπεύτρια Αποστολάκη Μαρία, η οποία μέσα από την
-          αγάπη της για τα παιδιά και γι’ αυτό που κάνει, στοχεύει στην βελτίωση
-          της ποιότητας της ζωής τόσο των παιδιών που παρουσιάζουν διαταραχές
-          λόγου και ομιλίας όσο και τις οικογένειες αυτών.
-        </p>
+    </section>
+  );
+}
+
       </div>
     </div>
   );
