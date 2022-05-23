a2c91854e3d38cd9de2c451559c4f114448d4668
Vaios Karampinis <oden@oden.gr>
2022-05-23 09:56:40 +0300

    body updates

--- components/Body.js
+++ components/Body.js
@@ -1,6 +1,6 @@
 export default function Body({ children }) {
   return (
-    <div className="container flex flex-col h-screen max-w-full text-xl bg-my-white text-my-base font-body">
+    <div className="flex flex-col h-screen max-w-full text-xl  bg-my-white text-my-base font-body">
       {children}
     </div>
   );
