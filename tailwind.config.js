9879f2673d4dfb4d4603a5f9540cda2391b75f96
Vaios Karampinis <oden@oden.gr>
2022-05-23 09:54:43 +0300

    basic colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        my: {
-          blue: "#2796d8",
-          green: "#93ad42",
+        color: {
+          black: "#333",
+          white: "#fff",
+          1: "#017DBB",
+          2: "#0FD0FF",
+          3: "#EC72DA",
+          4: "#F7AEF8",
         },
       },
       typography: (t) => ({
@@ -17,12 +21,12 @@ module.exports = {
           css: {
             color: "#333",
             "h1, h2, h3, h4, h5, h6": {
-              color: t("colors.pink.600"),
+              color: t("colors.color.black"),
             },
             a: {
-              color: t("colors.pink.600"),
+              color: t("colors.color.3"),
               "&:hover": {
-                color: t("colors.my.blue"),
+                color: t("colors.color.1"),
               },
             },
           },
