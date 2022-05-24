fd52102691d3f06d4c9d0fd7fbfc14c69a1c61ed
Vaios Karampinis <oden@oden.gr>
2022-05-24 09:22:53 +0300

    H Component

---
+++ components/H.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const headerClassMap = {
+  1: "text-4xl",
+  2: "text-3xl",
+  3: "text-2xl",
+  4: "text-xl",
+  5: "text-lg",
+  6: "text-base",
+};
+
+export default function H({ size, className = "", children }) {
+  return React.createElement(
+    `h${size}`,
+    { className: `${headerClassMap[size]} font-bold py-6 ${className}` },
+    children
+  );
+}
