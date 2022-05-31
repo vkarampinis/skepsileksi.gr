a76e524666b0b63b589aa121df63887a496cfa79
Vaios Karampinis <oden@oden.gr>
2022-05-31 20:30:38 +0300

    ContactSection

---
+++ components/ContactSection.js
@@ -0,0 +1,16 @@
+import H from "./H";
+import Highlight from "./Highlight";
+
+export default function ContactSection() {
+  return (
+    <section>
+      <div className="max-w-6xl p-4 mx-auto mt-24 ">
+        <H size={2}>Δωρεάν αξιολόγηση</H>
+        <p>
+          Ανεξάρτητα από το πού μένεις, η βοήθεια είναι πάντα{" "}
+          <Highlight>δίπλα σου!</Highlight>
+        </p>
+      </div>
+    </section>
+  );
+}
