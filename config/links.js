d8b3515f53610d4b772225807a54a3e9cb5c22e4
Vaios Karampinis <oden@oden.gr>
2022-05-31 20:27:00 +0300

    add new links

--- config/links.js
+++ config/links.js
@@ -1,9 +1,16 @@
 const links = {
-  Tiletherapia: "/tiletherapy",
+  Tiletherapia: "/tiletherapia",
   Kathodigisi: "/kathodigisi",
   Logotherapia: "/logotherapia",
   About: "/about",
   Arthra: "/arthra",
   Contact: "/contact",
+  FonologikiDiatarachi: "/logotherapia/fonologiki_diatarachi",
+  DiatarachesArthrosis: "/logotherapia/diataraches_arthrosis",
+  EidikiGlwsikiDiatarachiAntiliptouKaiEkfrastikouTupou:
+    "/logotherapia/eidiki_glwsiki_diatarachi_antiliptou_kai_ekfrastikou_tupou",
+  Autismos: "/logotherapia/autismos",
+  Traulismos: "/logotherapia/traulismos",
+  EidikesMathisiakesDiskolies: "/logotherapia/eidikes_mathisiakes_diskolies",
 };
 export default links;
