c3fb9effdd49c96e65175c9817a62cc12c685379
Vaios Karampinis <oden@oden.gr>
2022-05-31 17:31:09 +0300

    fix CTA responsiveness

--- components/Button.js
+++ components/Button.js
@@ -2,7 +2,7 @@ export default function Button({ className, pink, link, children }) {
   let color = pink ? "bg-color-1" : "bg-color-2";
   return (
     <a
-      className={`block px-4 py-2 text-center text-white ${color} ${className}`}
+      className={`px-4 py-2 text-center text-white  ${color} ${className}`}
       href={link}
     >
       {children}
--- components/CTA.js
+++ components/CTA.js
@@ -6,19 +6,16 @@ export default function CTA({ link, pink, linkText, children }) {
     <div className={`py-8 ${color}`}>
       <div className="flex flex-row max-w-6xl mx-auto">
         <p
-          className="z-10 block px-4 py-2 font-bold text-center basis-full text-color-white sm:basis-5/6 "
+          className="z-10 block px-4 py-2 font-bold text-center basis-full text-color-white lg:basis-5/6 "
           href={link}
         >
-          <span className="">{children}</span>
-          <Button
-            link={link}
-            pink={pink}
-            className="inline-block mt-4 sm:mt-0 sm:ml-10"
-          >
-            {linkText}
-          </Button>
+          <span className="block lg:inline-block">{children}</span>
+          <div className="m-auto mt-8 lg:inline-block lg:mt-0 lg:ml-10">
+            <Button link={link} pink={pink} className="">
+              {linkText}
+            </Button>
+          </div>
         </p>
-        <div className="sm:basis-1/6"></div>
       </div>
     </div>
   );
