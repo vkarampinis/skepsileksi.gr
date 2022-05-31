1dc1af8c51d7255390eafc43adb34d8459a78a9a
Vaios Karampinis <oden@oden.gr>
2022-05-31 16:33:43 +0300

    make CTA responsive

--- components/CTA.js
+++ components/CTA.js
@@ -6,15 +6,19 @@ export default function CTA({ link, pink, linkText, children }) {
     <div className={`py-8 ${color}`}>
       <div className="flex flex-row max-w-6xl mx-auto">
         <p
-          className="z-10 block px-4 py-2 font-bold text-center text-color-white basis-5/6 "
+          className="z-10 block px-4 py-2 font-bold text-center basis-full text-color-white sm:basis-5/6 "
           href={link}
         >
           <span className="">{children}</span>
-          <Button link={link} pink={pink} className="inline-block ml-10">
+          <Button
+            link={link}
+            pink={pink}
+            className="inline-block mt-4 sm:mt-0 sm:ml-10"
+          >
             {linkText}
           </Button>
         </p>
-        <div className="basis-1/6"></div>
+        <div className="sm:basis-1/6"></div>
       </div>
     </div>
   );
