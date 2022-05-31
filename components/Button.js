0737c939eb3e4a47dc357e6cdb53176d73f85682
Vaios Karampinis <oden@oden.gr>
2022-05-31 18:40:02 +0300

    button should not brake

--- components/Button.js
+++ components/Button.js
@@ -1,8 +1,8 @@
-export default function Button({ className, pink, link, children }) {
+export default function Button({ className = "", pink, link, children }) {
   let color = pink ? "bg-color-1" : "bg-color-2";
   return (
     <a
-      className={`px-4 py-2 text-center text-white  ${color} ${className}`}
+      className={`px-4 py-2 text-center text-white whitespace-nowrap ${color} ${className}`}
       href={link}
     >
       {children}
