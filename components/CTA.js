ec2522d6a649bb16262cacba8c1204839634cda9
Vaios Karampinis <oden@oden.gr>
2022-05-31 18:40:22 +0300

    remove href

--- components/CTA.js
+++ components/CTA.js
@@ -5,10 +5,7 @@ export default function CTA({ link, pink, linkText, children }) {
   return (
     <div className={`py-8 ${color}`}>
       <div className="flex flex-row max-w-6xl mx-auto">
-        <p
-          className="z-10 block px-4 py-2 font-bold text-center basis-full text-color-white lg:basis-5/6 "
-          href={link}
-        >
+        <p className="z-10 block px-4 py-2 font-bold text-center basis-full text-color-white lg:basis-5/6 ">
           <span className="block lg:inline-block">{children}</span>
           <div className="m-auto mt-8 lg:inline-block lg:mt-0 lg:ml-10">
             <Button link={link} pink={pink} className="">
