6758ecd84b0590806e4c39cf51b17078295b7ddc
Vaios Karampinis <oden@oden.gr>
2022-06-01 09:05:03 +0300

    add some padding

--- components/ContactSection.js
+++ components/ContactSection.js
@@ -4,7 +4,7 @@ import Highlight from "./Highlight";
 export default function ContactSection() {
   return (
     <section>
-      <div className="max-w-6xl p-4 mx-auto mt-24 ">
+      <div className="max-w-6xl p-4 mx-auto mt-24 prose prose-xl ">
         <H size={2}>Δωρεάν αξιολόγηση</H>
         <p>
           Ανεξάρτητα από το πού μένεις, η βοήθεια είναι πάντα{" "}
