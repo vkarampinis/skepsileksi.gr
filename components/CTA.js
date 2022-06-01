8963b465fa25f6082a2f37debd23cb17cbd69e7a
Vaios Karampinis <oden@oden.gr>
2022-06-01 09:01:41 +0300

    fix schemantic

--- components/CTA.js
+++ components/CTA.js
@@ -5,14 +5,14 @@ export default function CTA({ link, pink, linkText, children }) {
   return (
     <section className={`py-8 ${color}`}>
       <div className="flex flex-row max-w-6xl mx-auto">
-        <p className="z-10 block px-4 py-2 font-bold text-center basis-full text-color-white lg:basis-5/6 ">
+        <div className="z-10 block px-4 py-2 font-bold text-center basis-full text-color-white lg:basis-5/6 ">
           <span className="block lg:inline-block">{children}</span>
           <div className="m-auto mt-8 lg:inline-block lg:mt-0 lg:ml-10">
             <Button link={link} pink={pink} className="">
               {linkText}
             </Button>
           </div>
-        </p>
+        </div>
       </div>
     </section>
   );
