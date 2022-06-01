5f39fb3464d1d250c7da8dad7c3c058382f36dfb
Vaios Karampinis <oden@oden.gr>
2022-06-01 09:04:43 +0300

    add extra links

--- pages/logotherapia/index.js
+++ pages/logotherapia/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import BaseLayout from "../../components/BaseLayout";
 import ContactSection from "../../components/ContactSection";
+import CTALink from "../../components/CTALink";
 import H from "../../components/H";
 import TextSection from "../../components/TextSection";
 import links from "../../config/links";
@@ -46,6 +47,74 @@ export default function Logotherapia() {
             <Link href={links.EidikesMathisiakesDiskolies}>
               Ειδικές μαθησιακές δυσκολίες
             </Link>
+          </li>
+        </ul>
+      </TextSection>
+      <CTALink title="Τηλεθεραπεία" link={links.Tiletherapia}>
+        Η θεραπεία γίνεται εξ αποστάσεως μέσω του διαδικτύου
+      </CTALink>
+      <TextSection>
+        <H size={3}>Πότε πρέπει να συμβουλευτείτε έναν λογοθεραπευτή</H>
+        <p>
+          Οι γονείς θα πρέπει να συμβουλευτούν έναν λογοθεραπευτή οταν το παιδί
+          τους στην ηλικία των
+        </p>
+        <H size={4}>0 - 6 μηνών:</H>
+        <ul className="list-disc list-inside">
+          <li>δεν αντιδρά σε ήχους</li>
+          <li>δεν έχει βλεμματική επαφή</li>
+        </ul>
+        <H size={4}>6 - 12 μηνών:</H>
+        <ul className="list-disc list-inside">
+          <li>
+            δεν εξελίσσονται οι επικοινωνιακές δεξιότητες (αντίδραση σε ήχους
+            και βλεμματική επαφή)
+          </li>
+        </ul>
+        <H size={4}>12 – 18 μηνών:</H>
+        <ul className="list-disc list-inside">
+          <li>σταματήσει να μιλάει</li>
+          <li>η ομιλία του παραμένει στάσιμη</li>
+          <li>η ομιλία του χειροτερεύει</li>
+        </ul>
+        <H size={4}>18 – 24 μηνών:</H>
+        <ul className="list-disc list-inside">
+          <li>έχει περιορισμένο λεξιλόγιο</li>
+          <li>έχει δυσκατάληπτο ομιλία ( μη κατανοητός)</li>
+          <li>δεν μπορεί να σχηματίσει σύντομη πρόταση 2 λέξεων</li>
+        </ul>
+        <H size={4}>2 – 3 χρονών:</H>
+        <ul className="list-disc list-inside">
+          <li>η ομιλία παραμένει δυσκατάληπτη</li>
+          <li>δεν χρησιμοποιεί άρθρα</li>
+          <li>δεν σχηματίζει απλές προτάσεις</li>
+        </ul>
+        <H size={4}>Επίσης, ανησυχητικό είναι όταν:</H>
+        <ul className="list-disc list-inside">
+          <li>
+            παρουσιάζεται μειωμένη ικανότητα να παίρνει μέρος σε μια συζήτηση
+          </li>
+          <li>
+            αδυνατεί να εκφραστεί με ευκολία και ανάλογα με τις περιστάσεις
+          </li>
+        </ul>
+      </TextSection>
+      <TextSection>
+        <H size={2}>Χρήσιμες πληροφορίες</H>
+        <ul className="list-disc list-inside">
+          <li>
+            <Link href={links.FysiologikiAnaptiksiTouLogou}>
+              Φυσιολογική ανάπτυξη του λόγου και της ομιλίας
+            </Link>
+          </li>
+          <li>
+            <Link href={links.ProtiSinantisi}>Πρώτη συνάντηση</Link>
+          </li>
+          <li>
+            <Link href={links.SichnotitaSinedrion}>Συχνότητα συνεδριών</Link>
+          </li>
+          <li>
+            <Link href={links.SichnesErotisis}>Συχνές ερωτήσεις</Link>
           </li>
         </ul>
       </TextSection>
