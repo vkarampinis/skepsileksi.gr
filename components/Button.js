054be548921b7553721983b96479e4e2b434fb4e
Vaios Karampinis <oden@oden.gr>
2022-06-02 07:27:04 +0300

    wrap Button with Link component

--- components/Button.js
+++ components/Button.js
@@ -1,11 +1,14 @@
+import Link from "next/link";
+
 export default function Button({ className = "", pink, link, children }) {
   let color = pink ? "bg-color-1" : "bg-color-2";
   return (
-    <a
-      className={`px-4 py-2 text-center text-white whitespace-nowrap ${color} ${className}`}
-      href={link}
-    >
-      {children}
-    </a>
+    <Link href={link} prefetch>
+      <a
+        className={`px-4 py-2 text-center text-white whitespace-nowrap ${color} ${className}`}
+      >
+        {children}
+      </a>
+    </Link>
   );
 }
