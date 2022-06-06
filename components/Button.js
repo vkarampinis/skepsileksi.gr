cd352123a888a45f8f0cc56f6746f094db3e7772
Vaios Karampinis <oden@oden.gr>
2022-06-07 00:29:32 +0300

    remove prefetch

--- components/Button.js
+++ components/Button.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Button({ className = "", pink, link, children }) {
   let color = pink ? "bg-color-1" : "bg-color-2";
   return (
-    <Link href={link} prefetch>
+    <Link href={link}>
       <a
         className={`px-4 py-2 text-center text-white whitespace-nowrap ${color} ${className}`}
       >
