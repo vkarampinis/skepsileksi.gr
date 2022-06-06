60dfdfa436d4cea97dcc8f23834ab9cce09191e0
Vaios Karampinis <oden@oden.gr>
2022-06-06 20:52:38 +0300

    add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://skepsileksi.gr",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+};
+
+module.exports = config;
