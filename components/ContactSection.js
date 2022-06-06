89d72875041e44dd4675610574ec6784946d142e
Vaios Karampinis <oden@oden.gr>
2022-06-06 13:59:32 +0300

    create contact form

--- components/ContactSection.js
+++ components/ContactSection.js
@@ -1,15 +1,17 @@
+import ContactForm from "./ContactForm";
 import H from "./H";
 import Highlight from "./Highlight";
 
 export default function ContactSection() {
   return (
     <section>
-      <div className="max-w-6xl p-4 mx-auto mt-24 prose prose-xl ">
+      <div className="max-w-6xl p-4 mx-auto mt-10 prose prose-xl md:mt-24 ">
         <H size={2}>Δωρεάν αξιολόγηση</H>
         <p>
           Ανεξάρτητα από το πού μένεις, η βοήθεια είναι πάντα{" "}
           <Highlight>δίπλα σου!</Highlight>
         </p>
+        <ContactForm />
       </div>
     </section>
   );
