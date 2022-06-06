bf298a6d2442dd99968dfd857cffa7fec5526ef5
Vaios Karampinis <oden@oden.gr>
2022-06-06 13:57:12 +0300

    add extra info on footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,10 @@ export default function Footer() {
   return (
     <footer className="p-4 mt-10 bg-color-1">
       <div className="max-w-6xl mx-auto text-sm text-color-white ">
-        <p>Σκέψη - Λέξη © 2022</p>
+        <p>
+          Σκέψη - Λέξη © 2022 • Καλέστε μας για δωρεάν αξιολόγηση •{" "}
+          <a href="tel:6942414511">6942414511</a>
+        </p>
       </div>
     </footer>
   );
