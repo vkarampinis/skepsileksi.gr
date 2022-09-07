f3abf6f58f434b138584cea15f1c214ff3a930c5
Vaios Karampinis <oden@oden.gr>
2022-09-07 09:38:28 +0300

    MyTelephone component

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,12 @@
+import MyTelephone from "./MyTelephone";
+
 export default function Footer() {
   return (
-    <footer className="p-4 mt-10 bg-color-1">
-      <div className="max-w-6xl mx-auto text-sm text-color-white ">
+    <footer className="p-6 mt-10 bg-color-1">
+      <div className="max-w-6xl mx-auto text-lg text-color-white ">
         <p>
           Σκέψη - Λέξη © 2022 • Καλέστε μας για δωρεάν αξιολόγηση •{" "}
-          <a href="tel:6942414511">6942414511</a>
+          <MyTelephone />
         </p>
       </div>
     </footer>
---
+++ components/MyTelephone.js
@@ -0,0 +1,3 @@
+export default function MyTelephone() {
+  return <a href="tel:6942414511">694 24 14 511</a>;
+}
