f36c819ddc4dddf2ec9b0fd4cc0d7868bf950cfd
Vaios Karampinis <oden@oden.gr>
2024-08-28 23:27:52 +0300

    add table rendering

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -8,6 +8,22 @@ const MDXComponents = {
   h2: (props) => <H size={2} {...props} />,
   h3: (props) => <H size={3} {...props} />,
   h4: (props) => <H size={4} {...props} />,
+  table: (props) => (
+    <table
+      className="border border-gray-300 border-solid table-auto"
+      {...props}
+    />
+  ),
+  tr: (props) => <tr {...props} />,
+  th: (props) => (
+    <th className="px-3 py-2 text-center bg-slate-100" {...props} />
+  ),
+  td: (props) => (
+    <td
+      className="px-3 text-center border-r border-gray-300 border-solid last:border-r-0"
+      {...props}
+    />
+  ),
 };
 
 export default MDXComponents;
