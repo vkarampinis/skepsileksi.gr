ab43072714454c61a7938b860e53cf799644c8c1
Vaios Karampinis <oden@oden.gr>
2025-02-17 13:58:48 +0200

    style: Update MyTelephone component layout

--- components/MyTelephone.js
+++ components/MyTelephone.js
@@ -1,3 +1,6 @@
 export default function MyTelephone() {
-  return <a href="tel:6942414511">694 24 14 511</a>;
+  return (
+  <>
+  <nobr>🇬🇷 <a href="tel:6942414511">+30 694 24 14 511</a> (Viber)</nobr> <span className="hidden md:inline-block"> • </span> <nobr>🇩🇪 <a href="tel:6942414511">+49 176 58867736</a> (WhatsApp)</nobr>
+  </>);
 }
